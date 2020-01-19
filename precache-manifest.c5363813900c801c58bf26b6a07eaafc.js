a3b0c1b9d72e843406be289d59b22107ce201d88
unknown <danurey96@gmail.com>
2020-01-18 19:34:57 -0600

    Updates

---
+++ precache-manifest.c5363813900c801c58bf26b6a07eaafc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b846cd3cb902b4c25a675b1c2c9d71a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6034ce9e94c3530edfc7",
+    "url": "/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "28f88d6e7bf2a5d74419",
+    "url": "/static/js/2.d35a2c42.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d35a2c42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6034ce9e94c3530edfc7",
+    "url": "/static/js/main.5070fbc4.chunk.js"
+  },
+  {
+    "revision": "ed6c4f129452b2edba17",
+    "url": "/static/js/runtime-main.9a8399c6.js"
+  }
+]);
